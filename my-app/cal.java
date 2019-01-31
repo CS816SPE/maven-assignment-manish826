725aad899d71426c37d676699fe587c2bb0d8bed
manish826 <Manish.Kumar@iiitb.org>
2019-01-31 10:57:44 +0530

    maven_1

---
+++ my-app/cal.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+ public class caladd{
+   public static void main(String args[]){
+   float a,b,res;
+   char choice,ch;
+   Scanner scan=new Scanner(System.in);
+   do{
+      System.out.print("1. ADD TWO NUMBER\n");
+      System.out.print("2. SUB TWO NUMBER\n");
+      System.out.print("3. MUL TWO NUMBER\n");
+      System.out.print("4. Exit\n\n");
+      System.out.print("Enter YOur Choice : ");
+      choice = scan.next().charAt(0);
+      switch(choice){
+        case '1' : System.out.print("Enter Two Number : ");
+                   a=scan.nextFloat();
+                   b=scan.nextFloat();
+                   res=a+b;
+                   System.out.print("Result = " + res);
+                   break;
+       case '2' : System.out.print("Enter Two Number : ");
+                   a=scan.nextFloat();
+                   b=scan.nextFloat();
+                   res=a-b;
+                   System.out.print("Result = " + res);
+                   break;
+       case '3' : System.out.print("Enter Two Number : ");
+                   a=scan.nextFloat();
+                   b=scan.nextFloat();
+                   res=a*b;
+                   System.out.print("Result = " + res);
+                   break;
+       case '4' : System.exit(0);
+                  break;
+       default : System.out.print("INVALID CHOICE !!!");
+                 break;
+    }
+    System.out.print("\n--------------------------------\n");
+}
+while(choice!=4);
+}
+}
